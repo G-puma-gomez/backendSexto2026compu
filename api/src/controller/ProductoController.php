9c1a3746d015303a32aa0f979eb97418194013df
G-puma-gomez <gabrielpumagomez@gmail.com>
2026-06-17 17:41:06 -0400

    newCambio

--- api/src/controller/ProductoController.php
+++ api/src/controller/ProductoController.php
@@ -29,7 +29,34 @@ class ProductoController{
                     ]);
                 return;
             }
-        $producto=productos::update($id,$jsonData);
+            if(!isset($data['descripcion']) || trim($data['descripcion'])=="")
+                  {
+                echo json_encode(
+                    [
+                        "status"=>"error",
+                        "message"=>"la descripcion es obligatorio",
+                    ]);
+                return;
+            }
+        if(!isset($data['precio_unitario']) || trim($data['precio_unitario'])=="")
+                  {
+                echo json_encode(
+                    [
+                        "status"=>"error",
+                        "message"=>"el precio_unitario es obligatorio",
+                    ]);
+                return;
+            }
+        if(!isset($data['stock']) || trim($data['stock'])=="")
+                  {
+                echo json_encode(
+                    [
+                        "status"=>"error",
+                        "message"=>"el stock es obligatorio",
+                    ]);
+                return;
+            }
+        $producto=productos::update($id,$data);
         echo json_encode($producto);
     }
     // adicionar producto
--- api/src/models/productos.php
+++ api/src/models/productos.php
@@ -9,7 +9,24 @@ class productos
     }
     public static function update($id,$data)
     {
-        $sql = "SELECT * FROM productos";
-        return ConexionPDO::query($sql);//self::$users;
+        if(isset($data{'id'})){
+        unset($data['id']);
+        }
+        $campos=[];
+        $valores=[];
+        // construir datos
+        foreach($data as $columna=>$valor)
+            {
+                $campos[]="$columna=:columna";
+                $valores[":$columna"]=$valor;
+
+            }
+        $stringCampos=implode(",",$campos);
+        // preparamos la consulta 
+        $sql="UPDATE productos SET $stringCampos WRERE id=:id";
+        $valores[':id']=$id;
+       // $result=ConexionPDO::();
+       // $sql = "SELECT * FROM productos";
+        return $sql;// ConexionPDO::query($sql);//self::$users;
     }
 }
